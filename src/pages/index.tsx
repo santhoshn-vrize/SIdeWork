0c7a0880a155b771d825faa655fb4677b98c872c
SAN327905 <santhoshn641@gmail.com>
2023-11-10 18:31:00 +0530

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,31 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
-import Chart from '../Component/CustomChart';
-import Card from '../Component/Card'
-const inter = Inter({ subsets: ['latin'] });
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Chart from "../Component/CustomChart";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
- const labels = ['New RFQs','Ordered','Quoted RFQs','Order Lost','Declined RFQs','Cancelled']; // default label
+  const labels = [
+    "New RFQs",
+    "Ordered",
+    "Quoted RFQs",
+    "Order Lost",
+    "Declined RFQs",
+    "Cancelled",
+  ]; // default label
   const dataValues = [132, 130, 124, 115, 120, 126]; // default values
-  const activePrLabel="# RFQs";  // default value
+  const activePrLabel = "# RFQs"; // default value
   const title = "Summary of RFQs"; // default value
   return (
     <>
-      {/* <Card/> */}
-      <Chart labels={labels} dataValues={dataValues} activePrLabel={activePrLabel} title={title} />      
+      <Chart
+        labels={labels}
+        dataValues={dataValues}
+        activePrLabel={activePrLabel}
+        title={title}
+      />
     </>
   );
 };
